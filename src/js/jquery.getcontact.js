f0ab7f4ca7eaa8318967a27f912983e8d5af9d8a
baiyang <baiyang@ainemo.com>
2017-02-23 20:18:44 +0800

    ordinary commit

--- src/js/jquery.getcontact.js
+++ src/js/jquery.getcontact.js
@@ -2,7 +2,7 @@
 
     'use strict';
 
-    var methods = {
+    var TerminalSelect = {
 
         settings:{},
 
@@ -11,7 +11,7 @@
             var defaults = {
                 initData:[],
                 contactUrl:"",
-                inputName:"",
+                inputName:"terminals",
                 callback:{
                     beforeDelete: function(item){
                         //return false; 终端删除
@@ -23,6 +23,9 @@
                     onSelect: function(items){
                         //items,被选中的items
 
+                    },
+                    onfetchedUrlCalled : function(data){
+                        return data;
                     }
                 }
             };
@@ -30,20 +33,20 @@
 
             var settings = $.extend({},defaults, options);
 
-            methods.settings = settings;
+            TerminalSelect.settings = settings;
 
             return $(this).each (function(){
 
                 $(this).empty();
 
-                //1. create Dom fragment;
-                //========================
+                //Part1: terminal select Dom and event;
+                //1.1. create Dom fragment;
 
-                //1.1terminal Dom
-                var $terminalSubDom = $('<div class="ts-choose-list"><div class="ts-add-input-box"><input class="ts-input" id="tsAddInput" type="text" placeholder="这里是placeholder" /> <div class="ts-icon-more" data-toggle="modal" data-target="#addItemModal">加载更多</div> </div> <button class="ts-btn ts-btn-primary ts-btn-confirm">确定</button> </div>');
+                //1.1.1 terminal Dom
+                var $terminalSubDom = $('<div class="ts-choose-list"><div class="ts-add-input-box"><input class="ts-input" id="tsAddInput" type="text" placeholder="这里是placeholder" /> <div class="ts-icon-more" data-toggle="modal" data-target="#addItemModal"></div> </div> <button class="ts-btn ts-btn-primary ts-btn-confirm">确定</button> </div>');
 
 
-                //1.2 list data
+                //1.1.2 list data
                 var $terminalList = $('<div class="ts-select-list"></div>');
 
                 if(settings.initData.length > 0 ){ //初始化数据
@@ -52,7 +55,7 @@
 
                         var item = settings.initData[i];
 
-                        methods.addListItem(item,$terminalList);
+                        TerminalSelect.addListItem(item,$terminalList);
 
                     }
                 }
@@ -60,43 +63,20 @@
                 $(this).append($terminalSubDom).append($terminalList);
 
 
-                //2. create dialog fragment
+                //1.2 eventBind
+                //==============================
+                TerminalSelect.eventBind(settings);
 
+
+                //PART2: contact dialog dom and event;
+                //2.1  create dialog fragment
                 if($('#addItemModal').length === 0 ){
-                    var $addItemModal = '<div class="modal fade" id="addItemModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true"> <div class="modal-dialog"> <div class="modal-content"> <div class="modal-header"> <h4 class="modal-title" id="myModalLabel">添加组员</h4> </div> <div class="model-search"> <div class="input-group btn-search"> <input type="text" class="form-control search-input" id="searchText" placeholder="搜索"> <span class="input-group-btn"> <a class="btn btn-default search-btn"  id="searchItem"> <span class="glyphicon glyphicon-search"></span> </a> </span> </div> <div class="choose-bar"> <input type="checkbox" id="checkAll"> <label for="checkAll">全选</label> </div> </div> <div class="modal-body" id="addItemBody"> </div> <div class="modal-footer"> <button type="button" class="btn btn-default" data-dismiss="modal">取消</button> <button type="button" class="btn btn-primary" id="addItem">确定</button> </div> </div> </div> </div>';
+                    var $addItemModal = $('<div class="modal fade" id="addItemModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true"> <div class="modal-dialog"> <div class="modal-content"> <div class="modal-header"> <h4 class="modal-title" id="myModalLabel">添加组员</h4> </div> <div class="model-search"> <div class="input-group btn-search"> <input type="text" class="form-control search-input" id="searchText" placeholder="搜索"> <span class="input-group-btn"> <a class="btn btn-default search-btn"  id="searchItem"> <span class="glyphicon glyphicon-search"></span> </a> </span> </div> <div class="choose-bar"> <input type="checkbox" id="checkAll"> <label for="checkAll">全选</label> </div> </div> <div class="modal-body" id="addItemBody"> </div> <div class="modal-footer"> <button type="button" class="btn btn-default" data-dismiss="modal">取消</button> <button type="button" class="btn btn-primary" id="addItem">确定</button> </div> </div> </div> </div>');
                     $addItemModal.appendTo('body');
                 }
 
-
-                //eventBind
-                //==============================
-                methods.eventBind(settings);
-
-
-                //modal-dialog init
-                //===============================
-                if($.trim(settings.contactUrl) !== "") {
-                    try{
-                        $.getJSON(settings.contactUrl,function(data) {
-                            if(data == undefined) {
-                                return;
-                            }
-                            if(typeof(data) === "object") {
-                                AddItemModule.data = data;
-                            }
-                        }).fail(function(e) {
-                            if(window.console) {
-                                console.log('获取数据错误');
-                            }
-                        })
-                    }catch (e) {
-                        if(window.console) {
-                            console.log('获取数据错误');
-                        }
-                    }
-
-                }
-
+                //2.2. modal-dialog init
+                AddItemModule.init();
             });
         },
 
@@ -105,16 +85,16 @@
 
             $('.ts-btn-confirm').on('click',function(e) {
 
-                var inputVal = $('#tsAddInput').val();
+                var inputVal = $('#tsAddInput').val(),result;
 
                 if(settings.callback.beforeAddedFromInput) {
-                    var result = settings.callback.beforeAddedFromInput(inputVal);
+
+                    result = settings.callback.beforeAddedFromInput(inputVal);
                 }
 
                 if(typeof(result) === "object" && (!$.isEmptyObject(result))) {
 
-                    methods.addListItem(result);
-
+                    TerminalSelect.addListItem(result);
                 }
 
                 e.preventDefault();
@@ -130,7 +110,7 @@
                          return false;
                     }
                 }
-                methods.deleteListItem(id);
+                TerminalSelect.deleteListItem(id);
             })
         },
 
@@ -138,14 +118,14 @@
         addListItem : function(item,$parent) {
 
             // 去重
-            for(var i = 0 ;i < methods.selectedData.length ; i ++ ) {
-                if(methods.selectedData[i].id === item.id) {
+            for(var i = 0 ;i < TerminalSelect.selectedData.length ; i ++ ) {
+                if(TerminalSelect.selectedData[i].id === item.id) {
                     return ;
                 }
             }
 
             //data
-            methods.selectedData.push(item);
+            TerminalSelect.selectedData.push(item);
 
             //view
             var $parent = $parent ? $parent : $('.ts-select-list');
@@ -161,9 +141,9 @@
 
         deleteListItem : function(id) {
 
-            for(var i = 0 ;i < methods.selectedData.length; i++) {
-              if(methods.selectedData[i].id == id) {
-                  methods.selectedData.splice(i,1);
+            for(var i = 0 ;i < TerminalSelect.selectedData.length; i++) {
+              if(TerminalSelect.selectedData[i].id == id) {
+                  TerminalSelect.selectedData.splice(i,1);
               }
             }
 
@@ -174,11 +154,27 @@
 
 
         getSelected : function() {
-            return methods.selectedData;
+            return TerminalSelect.selectedData;
         },
 
-        createInput : function() {
+        createInputDom : function() {//return a input dom with selected id list as value
 
+            var values = TerminalSelect.selectedData.map(function(item){
+                return item.id;
+            });
+
+            if($('#tsHideInput').length > 0 ){
+                $('#tsHideInput').remove();
+            }
+
+            var $input = $('<input>',{
+                'hide':true,
+                'id':'tsHideInput',
+                'value':values,
+                'name':TerminalSelect.settings.inputName
+            });
+
+            return $(this).after($input);
         },
 
         selectedData :[]
@@ -187,17 +183,71 @@
 
     var AddItemModule = {
 
-        data : [],
+        contactData :{
+            isFetched:false,
+            data:{}
+        },
 
         //在弹出modal之前,先填充数据,如果使用模板可不调用fillModalData方法
         init : function(){
 
             $('#addItemModal').on('show.bs.modal',function(){
-                this.clearData();
-
-                this.fillModalData(data);
-                this.eventInit();
+                AddItemModule.clearData();
+                AddItemModule.fetchData();
+                AddItemModule.eventInit();
             })
+        },
+
+        fetchData : function() {
+            var contactData = AddItemModule.contactData;
+            if(contactData.isFetched === false) {
+                if($.trim(TerminalSelect.settings.contactUrl) !== "") {
+                    try{
+                        $.getJSON(TerminalSelect.settings.contactUrl,function(data) {
+
+                            var resultData = data;
+
+                            if(TerminalSelect.settings.callback.onfetchedUrlCalled) {
+                                resultData = TerminalSelect.settings.callback.onfetchedUrlCalled(data) ? TerminalSelect.settings.callback.onfetchedUrlCalled(data) :resultData;
+                            }
+
+
+                            if(resultData == undefined) {
+                                return;
+                            }
+
+                            if(typeof(resultData) === "string" || $.isArray(resultData) === true){
+                                AddItemModule.contactData = {
+                                    isFetched:true,
+                                    data:resultData
+                                };
+                            }else { //传入的数据不对
+                                if(window.console) {
+                                    console.log('传入或则返回的数据格式有误,请检查,只接受字符串或者数组类型的数据');
+                                }
+                                AddItemModule.contactData = {
+                                    isFetched:true,
+                                    data:"未能获取到数据"
+                                };
+                            }
+
+                            AddItemModule.fillModalData(AddItemModule.contactData.data);
+
+                        }).fail(function(e) {
+                            if(window.console) {
+                                console.log('获取数据错误');
+                                console.log(e);
+                            }
+                        })
+                    }catch (e) {
+                        if(window.console) {
+                            console.log('获取数据错误');
+                        }
+                    }
+                }
+            }else {
+                AddItemModule.fillModalData(AddItemModule.contactData.data);
+            }
         },
 
         clearData : function(){
@@ -205,8 +255,21 @@
         },
 
         fillModalData : function(initData){
+
+            //error scene : data is a string
+            if(typeof (initData) === "string") {//
+                $('#addItemBody').append($('<p>' +
+                    initData + '</p>'));
+                return;
+            }
+
+            //success scene : data is an array.
+
+            //todo 这里是否要进行容错?比如有些返回数据是nemoNumber,有些是number或者phone字段等等?
+
             var modalLists=$('<ul>',{"class":"modal-lists"}),
                 leni=initData.length;
+
             for(var i=0;i<leni;i++){
                 var itemGroup = $('<li>',{
                     "class":"item-group"
@@ -235,11 +298,16 @@
                         })).append(
                         $('<label>',{
                             "for":labelId,
-                            html:'<img class="item-img img-circle" src="' +obj.imgSrc+ '">' +
+                            "data-id":obj.id,
+                            "data-type":obj.type,
+                            "data-displayName":obj.name,
+                            "data-avatar":obj.avatar,
+                            "data-number":obj.number,
+                            html:'<img class="item-img img-circle" src="' +obj.avatar+ '">' +
                             '<div class="item-detail">' +
                             '<span class="item-name">' +obj.name+ '</span>' +
                             '<br/>' +
-                            '<span class="item-phone">'+obj.phone+'</span>' +
+                            '<span class="item-phone">'+obj.number+'</span>' +
                             '</div>'
                         })
                     ).appendTo(sul);
@@ -252,14 +320,17 @@
         //事件注册
         eventInit : function(){
             //1. 搜索
-            $('#searchItem').click(function(e){
+            $('#searchItem').off().on('click',function(e){
+
                 e.preventDefault();
-                var text = $('#searchText').val();
-                text = text.trim(' ');
+
+                var text = $.trim($('#searchText').val());
+
                 $('.item-detail').filter(function(){
+
                     var name = $(this).children('.item-name');
                     var phone = $(this).children('.item-phone');
-                    if(name.html().indexOf(text)>-1||phone.html().indexOf(text)>-1){
+                    if(name.html().indexOf(text) > -1 || phone.html().indexOf(text) > -1){
                         $(this).closest('li').show();
                     }else{
                         $(this).closest('li').hide();
@@ -289,20 +360,21 @@
                 e.stopPropagation();
             });
             //5. 确定
-            $('#addItem').click(function(){
+            $('#addItem').off().on('click',function(){
                 var results=[];
                 var lables= $('.item-checkbox').filter(':checked').siblings('label');
                 $.each(lables,function(index,item){
-                    var data = {};
-                    //data.imgSrc = $(item).children('.item-img').prop('src');
-                    data.name = $(item).find('.item-name').html();
-                    data.phone = $(item).find('.item-phone').html();
-                    methods.addListItem(data);
+
+                    var data = collectData(["id","displayName","type","number","avatar"],$(item));
+                    TerminalSelect.addListItem(data);
                     results.push(data);
                 });
-                if(settings.callback.onSelect) {
-                    settings.callback.onSelect(results);
+
+                if(TerminalSelect.settings.callback.onSelect) {
+                    TerminalSelect.settings.callback.onSelect(results);
                 }
+
+                $('#addItemModal').modal('hide');
 
             });
 
@@ -317,15 +389,24 @@
                     }
                 }
             });
+
+
+            function collectData(attrList,$item) { //["id","name","type","number","avatar"]
+                var item = {};
+                for(var i = 0 ;i < attrList.length; i ++) {
+                    item[attrList[i]] =$item.attr('data-' + attrList[i]);
+                }
+                return item;
+            }
         }
     };
 
 
     $.fn.terminalSelect = function(method) {
-        if(methods[method]) {
-            return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+        if(TerminalSelect[method]) {
+            return TerminalSelect[method].apply(this, Array.prototype.slice.call(arguments, 1));
         } else if (typeof method === 'object' || !method) {
-            return methods.init.apply(this, arguments);
+            return TerminalSelect.init.apply(this, arguments);
         } else {
             $.error( ' method  '+method+' is not exist.' );
         }
